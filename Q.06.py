b1ed27ca5a82b9e4cd8c288c7caa979a862f701a
ZeroOneKim <83469080+ZeroOneKim@users.noreply.github.com>
2021-07-17 16:51:34 +0900

    Add files via upload

---
+++ Q.06.py
@@ -0,0 +1,7 @@
+# what is true?
+print(None)
+print(1)
+print("")
+print(0)
+print(bool(0))
+#1= true
---
+++ Q.07.py
@@ -0,0 +1,3 @@
+#딕셔너리 키 중복
+d = {'height':180, 'weight':78, 'weight':84, 'temperature':36, 'eyesight':1}
+print(d['weight'])
