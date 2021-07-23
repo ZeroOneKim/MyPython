7c5a4321929ca58771ed5ca89fc6f00b37cff5a6
ZeroOneKim <83469080+ZeroOneKim@users.noreply.github.com>
2021-07-23 17:25:47 +0900

    Add files via upload

---
+++ Q.26.py
@@ -0,0 +1,20 @@
+planet = input('행성 입력 : ')
+
+if planet == '수성':
+    print('Mercury')
+elif planet == '금성':
+    print('Venus')
+elif planet == '지구':
+    print('Earth')
+elif planet == '화성':
+    print('Mars')
+elif planet == '목성':
+    print('Jupitar')
+elif planet == '토성':
+    print('Saturn')
+elif planet == '천왕성':
+    print('Uranos')
+elif planet == '해왕성':
+    print('Neptune')
+else:
+    print('잘못 입력하였습니다.')
