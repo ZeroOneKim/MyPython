eea932d4e5490972be3bca422b97a473c7733714
ZeroOneKim <83469080+ZeroOneKim@users.noreply.github.com>
2021-12-09 21:00:05 +0900

    Update and rename pygame.py to Mygame.py
    add feature
    crash ship => dead
    add a dead Sound
    add a Level Feature

--- Pygame/game/Mygame.py
+++ Pygame/game/Mygame.py
@@ -1,9 +1,23 @@
-import pygame
-import random
-import sys
+from os import close
+import pygame                    
+import random                   
+import sys                         
+import time                            
 
 def paintEntity(entity, x, y) : 
    monitor.blit(entity, (int(x), int(y)))
+
+def levelMessage(level):   #레벨 메시지 출력 함수
+    levelfont = pygame.font.Font('NanumGothic.ttf', 25)
+    text = levelfont.render(u'레벨 ' + str(level), True, (255-r, 255-g, 255-b))
+    monitor.blit(text, (swidth*0.45, sheight*0.01))
+    pygame.display.update()
+
+def bombMessage():          #**3** 사망 메시지에 대한 함수
+    myfont = pygame.font.Font('NanumGothic.ttf', 30)
+    text = myfont.render(u'Game Over!!', True, (255-r, 255-g, 255-b))
+    monitor.blit(text, (swidth*0.3, sheight*0.3))
+    pygame.display.update()
 
 def writeScore(score) :
     myfont = pygame.font.Font('NanumGothic.ttf', 20)    
@@ -11,7 +25,7 @@ def writeScore(score) :
     monitor.blit(txt, (10, sheight - 40))   
 
 def playGame() :
-    global monitor, ship, monster, missile 
+    global monitor, ship, monster, missile
 
     r = random.randrange(0, 256)
     g = random.randrange(0, 256)
@@ -19,18 +33,17 @@ def playGame() :
     
     shipX = swidth / 2 
     shipY = sheight * 0.8
-    dx, dy = 0, 0  
-    
+    dx, dy = 0, 0 
     monster = pygame.image.load(random.choice(monsterImage))
     monsterSize = monster.get_rect().size              
-    monsterX = 0 
-    monsterY = random.randrange(0, int(swidth * 0.3))
-    monsterSpeed = random.randrange(1, 5)
+    monsterX = random.randrange(0, int(swidth * 0.85))           #1  위에서 내려오게 밑 코드
+    monsterY = 0                                                #1  위에서 내려오게
+    monsterSpeed = 3
 
     missileX, missileY = None, None  
 
     fireCount = 0
-
+    Level = 0 
     while True :
         (pygame.time.Clock()).tick(50) 
         monitor.fill((r, g, b))             
@@ -61,16 +74,17 @@ def playGame() :
             shipY += dy
         paintEntity(ship, shipX, shipY)  
 
-        monsterX += monsterSpeed
-        if monsterX > swidth :
-            monsterX = 0
-            monsterY =random.randrange(0, int(swidth * 0.3))
+        monsterY += monsterSpeed            #1 Y로 지정해야 내려오게   
+
+        if monsterY > sheight :              #1 만약 Y값이 가로폭보다 크다면??
+            monsterX = random.randrange(0, int(swidth * 0.85))   #1 X좌표 아무데서나 떨어지게
+            monsterY = 0
             monster = pygame.image.load(random.choice(monsterImage))
             monsterSize = monster.get_rect().size
-            monsterSpeed = random.randrange(1, 5)
-           
+            monsterSpeed = Level + 1
+            
+            
         paintEntity(monster, monsterX, monsterY)
-        
         if missileX != None :                         
             missileY -= 10
             if missileY < 0 :
@@ -80,38 +94,44 @@ def playGame() :
             if (monsterX < missileX and missileX < monsterX + monsterSize[0]) and \
                    (monsterY < missileY and missileY < monsterY + monsterSize[1]) :
                 fireCount += 1
-
+                if fireCount >= 5:
+                    Level = int(fireCount/5) #5점을 획득하면 레벨이 1 증가
                 monster = pygame.image.load(random.choice(monsterImage))
                 monsterSize = monster.get_rect().size
-                monsterX = 0
-                monsterY =random.randrange(0, int(swidth * 0.3))
-                monsterSpeed = random.randrange(1, 5)
+                monsterX = random.randrange(0, int(swidth * 0.85))   #1 맞았을 때도 변경 해주게끔
+                monsterY = 0
+                monsterSpeed = Level + 1  # 레벨기능 
                 
-             
                 missileX, missileY= None, None   
-
-        writeScore(fireCount)
         
+        if (monsterX < shipX and shipX < monsterX + monsterSize[0]) and \
+            (monsterY < shipY and shipY < monsterY + monsterSize[1]): #**4**
+            endgame.play()
+            bombMessage()      #터질때,
+            time.sleep(5)  
+            close()
+        writeScore(fireCount)
+        levelMessage(Level)
+
         pygame.display.update()
 
 r, g, b = [0] * 3               
 swidth, sheight = 500, 700 
-monitor = None              
+monitor = None 
 ship, shipSize = None, 0    
 
 monsterImage = ['monster01.png', 'monster02.png', 'monster03.png', 'monster04.png', \
                 'monster05.png', 'monster06.png', 'monster07.png', 'monster08.png', \
                 'monster09.png', 'monster10.png']
+
 monster = None 
-
 missile = None   
-
-
 pygame.init()
 monitor = pygame.display.set_mode((swidth, sheight))
-pygame.display.set_caption('우주괴물 무찌르기')
 
-ship = pygame.image.load('ship02.png')
+pygame.display.set_caption('우주괴물 무찌르기')
+endgame = pygame.mixer.Sound('Bang.wav')    #4  endgame에 사운드 넣기
+ship = pygame.image.load('ship01.png')
 shipSize = ship.get_rect().size
 
 missile = pygame.image.load('missile.png')
