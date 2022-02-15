49430bbda389149262b88e37ecf0af74a1d1c99b
YungIl Kim <83469080+ZeroOneKim@users.noreply.github.com>
2022-02-15 16:06:09 +0900

    Delete Django/DjangoGbook directory

--- Django/DjangoGbook/guestbook/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from guestbook.models import Guestbook #모델.py에서 만든 내용을 admin에 적용
-
-class GuestbookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'passwd', 'content')
-
-admin.site.register(Guestbook, GuestbookAdmin)
--- Django/DjangoGbook/myproject/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    #template을 해석해서 html의 코드를 생성해주는 함수
-    return render(request, 'index.html')
